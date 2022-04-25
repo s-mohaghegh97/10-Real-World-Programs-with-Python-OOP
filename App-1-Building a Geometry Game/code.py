6ff58c702225456cf140f0d029c10a85c3d56082
Sajjad Mohaghegh <s.mohaghegh97@gmail.com>
2022-04-25 23:03:50 +0430

    Add feature to App1.
    Add graphical interface to app1.

--- App-1-Building a Geometry Game/code.py
+++ App-1-Building a Geometry Game/code.py
@@ -1,4 +1,5 @@
 from random import randint
+import turtle
 
 
 class Point:
@@ -28,25 +29,56 @@ class Rectangle:
         return (self.upright.x - self.lowleft.x) * (self.upright.y - self.lowleft.y)
 
 
-rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
-                      Point(randint(10, 19), randint(10, 19)))
+class GuiRectangle(Rectangle):
+
+    def draw(self, canvas):
+        canvas.penup()
+        canvas.goto(self.lowleft.x, self.lowleft.y)
+
+        canvas.pendown()
+        canvas.forward(self.upright.x - self.lowleft.x)
+        canvas.left(90)
+        canvas.forward(self.upright.y - self.lowleft.y)
+        canvas.left(90)
+        canvas.forward(self.upright.x - self.lowleft.x)
+        canvas.left(90)
+        canvas.forward(self.upright.y - self.lowleft.y)
+
+
+class GuiPoint(Point):
+
+    def draw(self, canvas, size=5):
+        canvas.penup()
+        canvas.goto(self.x, self.y)
+        canvas.pendown()
+        canvas.dot(size=size)
+
+
+rec = GuiRectangle(Point(randint(0, 200), randint(0, 200)),
+                   Point(randint(200, 400), randint(200, 400)))
 
 print("Rectangle Coordinates: ",
-      rectangle.lowleft.x, ",",
-      rectangle.lowleft.y, "and",
-      rectangle.upright.x, ",",
-      rectangle.upright.y
+      rec.lowleft.x, ",",
+      rec.lowleft.y, "and",
+      rec.upright.x, ",",
+      rec.upright.y
       )
 
-user_point = Point(
+user_point = GuiPoint(
     float(input("Guess X: ")),
     float(input("Guess Y: "))
 )
 
-user_area = float(input("Guess rectangle area: "))
+user_area = float(input("Guess rec area: "))
 
-print("Your point was inside rectangle: ",
-      user_point.falls_in_rectangle(rectangle))
+print("Your point was inside rec: ",
+      user_point.falls_in_rectangle(rec))
 
 print("Your area was off by: ",
-      rectangle.area() - user_area)
+      rec.area() - user_area)
+
+myturtle = turtle.Turtle()
+rec.draw(canvas=myturtle)
+user_point.draw(canvas=myturtle)
+
+turtle.done()
