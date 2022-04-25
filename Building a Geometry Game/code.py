02637ca39dab412f969e2041b3e575175c895132
Sajjad Mohaghegh <s.mohaghegh97@gmail.com>
2022-04-25 15:45:47 +0430

    develop Building a Geometry Game (App1).
    create two class for point and rectangle objects.
    develop several methods for more learning.

---
+++ Building a Geometry Game/code.py
@@ -0,0 +1,52 @@
+from random import randint
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.lowleft.x < self.x < rectangle.upright.x \
+                and rectangle.lowleft.y < self.y < rectangle.upright.y:
+            return True
+        else:
+            return False
+
+    def distance_from_point(self, point):
+        return ((self.x - point.x) ** 2 + (self.y - point.y) ** 2) ** 0.5
+
+
+class Rectangle:
+
+    def __init__(self, lowleft, upright):
+        self.lowleft = lowleft
+        self.upright = upright
+
+    def area(self):
+        return (self.upright.x - self.lowleft.x) * (self.upright.y - self.lowleft.y)
+
+
+rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
+                      Point(randint(10, 19), randint(10, 19)))
+
+print("Rectangle Coordinates: ",
+      rectangle.lowleft.x, ",",
+      rectangle.lowleft.y, "and",
+      rectangle.upright.x, ",",
+      rectangle.upright.y
+      )
+
+user_point = Point(
+    float(input("Guess X: ")),
+    float(input("Guess Y: "))
+)
+
+user_area = float(input("Guess rectangle area: "))
+
+print("Your point was inside rectangle: ",
+      user_point.falls_in_rectangle(rectangle))
+
+print("Your area was off by: ",
+      rectangle.area() - user_area)
